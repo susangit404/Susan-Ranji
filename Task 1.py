ac86db1e609987e6a89c8b751b11bb6a2918b5f1
susangit404 <susan.ranji254@gmail.com>
2025-07-03 16:21:44 +0300

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+
+#Addition, Subtraction, Multiplication, Division of int a & b
+
+a = input("enter first number: ")
+b = input("enter second number: ")
+a = int(a)
+b = int(b)
+print('Addition: ',a+b)
+print('Subtraction: ',a-b)
+print('Multiplication: ',a*b)
+print('Division: ', a/b)
+
+
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+# Hello John Doe!
+
+a = input("Enter your first name: ")
+b = input("Enter your second name: ")
+print("Hello, " + a + " " + b + "!" + " Welcome to the Python Programme.") 
